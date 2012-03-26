c28852e45640863e06163e0948826271756e0b5a
Pierre Pronchery <khorben@defora.org>
2012-03-27 01:00:17 +0200

    No longer assume that hidden keywords are not set already

--- src/target/firmware/include/defines.h
+++ src/target/firmware/include/defines.h
@@ -2,17 +2,29 @@
 #ifndef _DEFINES_H
 #define _DEFINES_H
 
-#define __attribute_const__	__attribute__((__const__))
+#ifndef __attribute_const__
+# define __attribute_const__	__attribute__((__const__))
+#endif
 
 /* type properties */
-#define __packed		__attribute__((packed))
-#define __aligned(alignment)	__attribute__((aligned(alignment)))
-#define __unused		__attribute__((unused))
+#ifndef __packed
+# define __packed		__attribute__((packed))
+#endif
+#ifndef __aligned
+# define __aligned(alignment)	__attribute__((aligned(alignment)))
+#endif
+#ifndef __unused
+# define __unused		__attribute__((unused))
+#endif
 
 /* linkage */
-#define __section(name) __attribute__((section(name)))
+#ifndef __section
+# define __section(name) __attribute__((section(name)))
+#endif
 
 /* force placement in zero-waitstate memory */
-#define __ramtext __section(".ramtext")
+#ifndef __ramtext
+# define __ramtext __section(".ramtext")
+#endif
 
 #endif /* !_DEFINES_H */
